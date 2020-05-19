61754e060e571aa17b2688ca462774084ac56167
Anselm Hahn <Anselm.Hahn@gmail.com>
2020-05-19 12:01:06 +0200

    Class-Patern is done, now function-pattern

--- copy2hash/copy2hash.py
+++ copy2hash/copy2hash.py
@@ -11,8 +11,8 @@
 
 import argparse
 import hashlib as hlib
-from pathspec import Path
-import os
+from pathlib import Path
+from shutil import copyfile
 import sys
 
 # imported fileformats
@@ -35,9 +35,12 @@ def log(msg, mode=None):
     msg : str
         Message to print to the terminal.
     mode : int, optional
-        If mode is activated, message becomes an error message.
+        If mode is activated, message becomes an error message for 1 or verbose message 
+        for 2.
     """
-    if mode:
+    if mode == 1:
+        print(f"[ERROR] {msg}")
+    elif mode == 2:
         print(f"[ERROR] {msg}")
     else:
         print(msg)
@@ -48,20 +51,76 @@ if sys.version < "3.6":
     sys.exit(1)
 
 
+class SHAKeys:
+    sha1 = "sha1"
+    sha224 = "sha224"
+    ssa256 = "ssa256"
+    sha384 = "sha384"
+    sha512 = "sha512"
+    blake2b = "blake2b"
+    blake2s = "blake2s"
+    md5 = "md5"
+    sha3_224 = "sha3_224"
+    sha3_256 = "sha3_256"
+    sha3_384 = "sha3_384"
+    sha3_512 = "sha3_512"
+    shake_128 = "shake_128"
+    shake_256 = "shake_256"
+
+
 class ExportReport:
-    pass
+    def __init__(self, args):
+        self.args = args
 
 
 class HashTag:
-    pass
+    def __init__(self, args):
+        self.args = args
 
 
-class CopyFile(ExportReport, HashTag):
-    pass
+class Copy2Hash(ExportReport, HashTag):
+    _copy_dir = {"index":[], "mode": [], "filename": []}    
 
+    def __init__(self, args):
 
-def copy2hash(fnames, args):
-    pass
+        super().__init__(args)
+        self.args = args
+    
+    def hallo(self):
+        print("hallo", self.args)
+
+    def get_files(self):
+        """Get the file names and save it.
+
+        get_files() reads the filenames and the working mode to an internal dictionary.
+        This is part I of II, because get_hash() has to add the hash-keys according to 
+        the number of selected SHA keys in the parser. 
+        """        
+        for i, file in enumerate(self.args["infile"]):
+            self._copy_dir["index"].append(i)
+            if self.args["move"]:
+               self._copy_dir["mode"].append("move")
+            else:
+                self._copy_dir["mode"].append("copy")
+            self._copy_dir["filename"].append(file)
+            
+            self.hallo()
+            #self._copy_dir[SHAKeys.blake2s] = file
+    
+    def get_hash(self):
+        
+        print(self._copy_dir)
+
+    
+    def ext_dir(self):
+        directory = Path(self.args["direcotry"])
+        if not directory.is_dir():
+            directory.mkdir(parents=True, exist_ok=True)
+            if self.args["verbose"]:
+                log(f"Made directory {directory}!", 2)
+    
+    def initialise(self):
+        pass
 
 
 def get_args(opt=None):
@@ -93,7 +152,7 @@ def get_args(opt=None):
         ),
     )
     parser.add_argument(
-        "-rp",
+        "-r",
         "--report",
         help=(
             "define one or a series of file format(s) for the rename-report(s) to "
@@ -101,7 +160,7 @@ def get_args(opt=None):
             "are: csv, json, pkl, txt, yaml, xml, or own file extension as ASCII; "
             "default is json"
         ),
-        default="json",
+        default=["json"],
         nargs="*",
         type=str,
     )
@@ -115,7 +174,7 @@ def get_args(opt=None):
             "sha3_224, sha3_256, sha3_384, sha3_512, shake_128, shake_256; default "
             "sha256"
         ),
-        default="sha256",
+        default=["sha256"],
         nargs="*",
         type=str,
     )
@@ -128,6 +187,7 @@ def get_args(opt=None):
             "can required sudo rights."
         ),
         default=None,
+        nargs=1,
         type=str,
     )
     parser.add_argument(
@@ -140,12 +200,22 @@ def get_args(opt=None):
         action="store_true",
     )
     parser.add_argument(
-        "-fxt", "--file_extension", help=("replaced the given file extension by the "
-        "abbreviations of the used secure hash algorithms (sha)"),
-        action="store_false"
+        "-fxt",
+        "--file_extension",
+        help=(
+            "replaced the given file extension by the abbreviations of the used secure "
+            "hash algorithms (sha)"
+        ),
+        action="store_false",
     )
     parser.add_argument(
-        "-ver", "--verbose", help=("enable the verbose mode"), action="store_false"
+        "-nfxt",
+        "--no_file_extension",
+        help=("removed the given file extension by the nothing"),
+        action="store_false",
+    )
+    parser.add_argument(
+        "-ver", "--verbose", help=("enable the verbose mode"), action="store_true"
     )
     parser.add_argument(
         "-v",
@@ -170,9 +240,11 @@ def command_line_runner():
     if not args["infile"]:
         log("Missing input file(s)!", mode=1)
         return
-
-    fnames = args["infile"]
-    copy2hash(fnames, args)
+    
+    if args["directory"]:
+        args["directory"] = Path(args["directory"])
+    Copy2Hash(args).get_files()
+    
 
 
 if __name__ == "__main__":
